4ffa07f178048755d9f478070cbc90a17f029173
Vinícius Blandy <viny19381@hotmail.com>
2023-10-18 20:43:51 -0300

    Terminando home e tech

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,29 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
+const imagemin = require('gulp-imagemin')
 
 function compilaSass(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename('main.min.css'))
     .pipe(gulp.dest('./public/css'))
 }
 
-exports.default = compilaSass
+function compilaJavascript(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('./public/script'))
+}
+
+function compilaImg(){
+    return gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./public/img'))
+}
+
+
+
+exports.default = gulp.parallel(compilaJavascript, compilaSass, compilaImg)
