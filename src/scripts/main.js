c7d988c52028fb9b429a689d9acd4b57951f3af9
Vinícius Blandy <viny19381@hotmail.com>
2023-10-22 08:43:59 -0300

    Terminando projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,8 +17,61 @@ hamburger.addEventListener('click', (e) => {
 document.addEventListener('scroll', () => {
     if (window.scrollY >= 576) {
         menu.classList.remove('active')
-        list.forEach(item =>{
+        hamburger.classList.remove('active')
+        list.forEach(item => {
             item.style.animation = ''
         })
     }
 })
+
+const digitando = document.querySelector('.digitando')
+function letraShow() {
+    const arrTexto = digitando.innerHTML.split('')
+    digitando.innerHTML = ''
+    arrTexto.forEach((letra, index) => {
+        setTimeout(() => {
+            digitando.innerHTML += letra
+        }, 75 * index)
+    })
+}
+
+letraShow()
+
+const buttons = document.querySelectorAll('.projectbutton')
+const lista = document.querySelectorAll('.portfolio__armazenamento__list__item')
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const alvo = e.target
+
+        buttons.forEach(button => {
+            if (button !== alvo) {
+                button.classList.remove('active')
+            }
+        })
+        alvo.classList.add('active')
+
+
+        lista.forEach(item =>{
+            if (alvo.id == 'todos' || alvo.id == 'frontend' && item.classList.contains('frontend') || alvo.id == 'backend' && item.classList.contains('backend')){
+                item.style.display = 'block'
+                item.style.animation = 'teste forwards .7s ease-in'
+                if(alvo.id == 'todos'){
+                    item.style.animation = 'animaImg forwards .7s ease-in'
+                }
+            } else{
+                item.style.display = 'none'
+                item.style.animation = ''
+            }
+        })           
+    })
+})
+
+
+// if (alvo.id === 'todos' ||
+//             (alvo.id === 'frontend' && item.classList.contains('frontend')) ||
+//             (alvo.id === 'backend' && item.classList.contains('backend'))) {
+//                 // Adicione a animação de novo
+                
+//             } else {
+                
+//             }
